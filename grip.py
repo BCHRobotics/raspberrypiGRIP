2a5f2c002a54ee21add7edb2b5a8dede0b5f4d20
AlliedAttendant <46505117+AlliedAttendant@users.noreply.github.com>
2019-02-14 14:25:10 -0800

    Update grip.py

--- grip.py
+++ grip.py
@@ -13,19 +13,6 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.vision.VisionRunner;
 import edu.wpi.first.wpilibj.vision.VisionThread;
-public class Test {
-        public static void main(String] args){
-
-
-                VisionThread visionThread;
-             
-                Object imgLock = new Object();
-                System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-                NetworkTable table = NetworkTable.getTable("foo");
-                Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-                System.out.println("mat = " + mat.dump());
-                UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-               camera.setResolution(640, 480);
 import cv2
 import numpy
 import math
